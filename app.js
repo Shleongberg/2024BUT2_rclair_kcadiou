2a5fe02fecff61a17c86c32fb525105bb0c9a65f
Shleongberg <r3d32005@gmail.com>
2024-10-09 08:54:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    let data = {
-        prenom: "Alice",
-        nom: "Dumont"
-    };
+    let data = { prenom: "Alice", nom: "Dumont" };
     res.render("index", data);
 })
 
-
+app.use((req, res) => {
+    res.status(404).render("404");
+})
 
 server.listen(3000, () => {
     console.log('Server running on port 3000');
-    });
-    
+});
+
 
