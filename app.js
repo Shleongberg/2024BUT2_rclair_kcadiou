59b41d70b5b8081883660e9ca486e4f5cbf33b64
Shleongberg <r3d32005@gmail.com>
2024-10-14 16:23:59 +0200

    nouvelle page

--- app.js
+++ app.js
@@ -10,51 +10,21 @@ app.use(express.static('public'));
 app.get('/', async function(req, res) {
     try{
         const users  = await utilisateurs.getUserById(1);
-        console.log(users);
         res.render("index",users);
     } catch (err){
-        console.log(err);
         res.status(500).send('Erreur lors de la récupération des données')
         
     }
 })
-app.get('/login', async function(req, res) {
-    try{
-        const users  = await utilisateurs.getUserById(1);
-        console.log(users);
-        res.render("login",users);
-    } catch (err){
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données')
-        
-    }
+app.get('/login', function(req, res) {
+   res.render("login");
 })
 app.get('/catalogue', async function(req, res) {
-    try{
-        const users  = await utilisateurs.getUserById(1);
-        console.log(users);
-        res.render("catalogue",users);
-    } catch (err){
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données')
-        
-    }
+        res.render("catalogue");
 })
 
 app.get('/produit', async function(req, res) {
-    try{
-        const users  = await utilisateurs.getUserById(1);
-        console.log(users);
-        res.render("product",users);
-    } catch (err){
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données')
-        
-    }
-})
-
-app.use((req, res) => {
-    res.sendFile("./public/css/style.css", { root: __dirname });
+        res.render("product");
 })
 
 
